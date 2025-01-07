118a1a601208667120bf93363a09ecdb87175936
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-06 21:49:58 -0330

    Read Players Inventory

--- WizardInventory.py
+++ WizardInventory.py
@@ -1,6 +1,8 @@
 def main():
- game_items = ReadGameItems 
- ShowGameItems()
+ game_items = ReadGameItems()
+ player_inventory = ReadPlayerInventory()
+ #ShowGameItems(game_items)
+ ShowPlayerInventory(player_inventory)
 
 
 def ReadGameItems():
@@ -15,7 +17,22 @@ def ReadGameItems():
   print("File wasn't found, Ensure file is in directory")  
  
 
+def ReadPlayerInventory():
+ player_inventory = []
+ try:
+  with open("wizard_inventory.txt") as file:
+   for line in file:
+    line = line.replace('\n', '').strip()
+    player_inventory.append(line)
+  return player_inventory
+ except FileNotFoundError:
+  print("File wasn't found, Ensure file is in directory")
+
 def ShowGameItems(game_items):
  for i, items in enumerate(game_items, start=1):
   print(i, items)
- main()
+
+def ShowPlayerInventory(player_inventory):
+ for i, items in enumerate(player_inventory, start=1):
+  print(i, items)
+main()
