af8e1da090f7bcc32d3d3405613dc2fb9546bb0d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-07 20:38:00 -0330

    drop an item from inventory

--- WizardInventory.py
+++ WizardInventory.py
@@ -1,8 +1,15 @@
 def main():
- game_items = ReadGameItems()
- player_inventory = ReadPlayerInventory()
- #ShowGameItems(game_items)
- ShowPlayerInventory(player_inventory)
+ while True:
+  game_items = ReadGameItems()
+  player_inventory = ReadPlayerInventory()
+  DisplayCommands()
+  command = input("Enter a command: ")
+  if command == "1":
+    pass
+  elif command == "2":
+    ShowPlayerInventory(player_inventory)
+  elif command == "3":
+    DropItem(player_inventory)
 
 
 def ReadGameItems():
@@ -28,16 +35,35 @@ def ReadPlayerInventory():
  except FileNotFoundError:
   print("File wasn't found, Ensure file is in directory")
 
-def ShowGameItems(game_items):
- for i, items in enumerate(game_items, start=1):
-  print(i, items)
-
 def ShowPlayerInventory(player_inventory):
- for i, items in enumerate(player_inventory, start=1):
+ for i, items in enumerate(player_inventory):
   print(i, items)
 
 def SaveInventory(player_inventory):
  with open("wizard_inventory.txt") as file:
   for items in player_inventory:
    file.write(f"{items}\n")
+
+def DropItem(player_inventory):
+ while True:
+  ShowPlayerInventory(player_inventory)
+  try:
+    user_number = int(input("Number for item to delete: "))
+    if user_number < 1 or user_number > len(player_inventory):
+     print("Invaild number for inventory")
+    else:
+     item = player_inventory.pop(user_number-1)
+     print(f"{item} was dropped from the inventory.")
+     SaveInventory(player_inventory)
+     break
+  except ValueError:
+    print("Enter a vaild number (1-4)")
+
+
+def DisplayCommands():
+ print("COMMAND MENU")
+ print("1 - Walk down the path")
+ print("2 - Show your items")
+ print("3 - Drop an item")
+ print("4 - Exit program")
 main()
