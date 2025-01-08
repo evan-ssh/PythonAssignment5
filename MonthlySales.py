6d53cdc7ac34293a958c8b1366ef8837ce72cdcb
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-08 19:29:57 -0330

    Display Menu

---
+++ MonthlySales.py
@@ -0,0 +1,6 @@
+def DisplayMenu():
+ print("COMMAND MENU")
+ print("Monthly - View monthly sales")
+ print("Yearly - View yearly summary")
+ print("Edit - Edit sales for a month")
+ print("Exit - Exit program")
