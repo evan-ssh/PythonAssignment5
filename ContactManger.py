3540705399fdd63d22092b63b92959eef600d1c9
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-07 21:38:53 -0330

    display menu

---
+++ ContactManger.py
@@ -0,0 +1,9 @@
+def DisplayMenu():
+ print()
+ print("1 - Display all contacts")
+ print("2 - View Contact")
+ print("3 - Add a contact")
+ print("4 - Delete a contact")
+ print("5 - Exit program")
+
+ 
